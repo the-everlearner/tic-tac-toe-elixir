a0aef9c8fcbf7ad90cb1056dcb9b288d46110c17
the-everlearner <devlin.glasman@gmail.com>
2019-01-09 12:18:40 +0000

    Add analysis of win lines for rows and cols

--- lib/board.ex
+++ lib/board.ex
@@ -11,4 +11,50 @@ defmodule Board do
   def full?(board) do
     !Enum.member?(board, :empty_mark)
   end
+
+  def won?(board, player_mark) do
+    all_indices = get_all_indices(board)
+    win_lines = Enum.map(all_indices, fn index_set -> get_marks(index_set, board) end)
+    assessed_lines = Enum.map(win_lines, fn line -> line_won?(line, player_mark) end)
+    Enum.any?(assessed_lines, fn assessment -> assessment == true end)
+  end
+
+  def line_won?(line, player_mark) do
+    Enum.all?(line, fn mark -> mark == player_mark end)
+  end
+
+  def get_all_indices(board) do
+    row_indices(board) ++ col_indices(board)
+  end
+
+  def row_indices(board) do
+    board_indices = 0..length(board)
+    Enum.chunk_every(board_indices, dimension(board), dimension(board), :discard)
+  end
+
+  def col_indices(board) do
+    starting_points = 0..(dimension(board) - 1)
+    first_col = Enum.map(starting_points, fn point -> point * dimension(board) end)
+    second_col = Enum.map(starting_points, fn point -> point * dimension(board) + 1 end)
+    third_col = Enum.map(starting_points, fn point -> point * dimension(board) + 2 end)
+    [first_col, second_col, third_col]
+  end
+
+  def get_marks(indices, board) do
+    Enum.map(indices, fn index ->
+      get_mark(board, index)
+    end)
+  end
+
+  def get_mark(board, index) do
+    Enum.at(board, index)
+  end
+
+  def all_player_marks?(marks, player_mark) do
+    Enum.all?(marks, fn mark -> mark == player_mark end)
+  end
+
+  def dimension(board) do
+    trunc(:math.sqrt(length(board)))
+  end
 end
--- test/board_test.exs
+++ test/board_test.exs
@@ -18,6 +18,88 @@ defmodule BoardTest do
 
   test "returns true if board full" do
     full_board = List.duplicate(player_one_mark(), 9)
-    assert full?(full_board)
+    assert full?(full_board) == true
+  end
+
+  test "get board dimension" do
+    assert dimension(@empty_board) === 3
+  end
+
+  test "returns false if board not won" do
+    refute won?(@empty_board, player_one_mark())
+  end
+
+  test "top row win" do
+    board = generate_marked_board(@empty_board, [0, 1, 2], player_one_mark())
+
+    assert won?(board, player_one_mark())
+  end
+
+  test "mid row win" do
+    board = generate_marked_board(@empty_board, [3, 4, 5], player_one_mark())
+
+    assert won?(board, player_one_mark())
+  end
+
+  test "bot row win" do
+    board = generate_marked_board(@empty_board, [6, 7, 8], player_one_mark())
+
+    assert won?(board, player_one_mark())
+  end
+
+  test "first col win" do
+    board = generate_marked_board(@empty_board, [0, 3, 6], player_one_mark())
+
+    assert won?(board, player_one_mark())
+  end
+
+  test "mid col win" do
+    board = generate_marked_board(@empty_board, [1, 4, 7], player_one_mark())
+
+    assert won?(board, player_one_mark())
+  end
+
+  test "end col win" do
+    board = generate_marked_board(@empty_board, [2, 5, 8], player_one_mark())
+
+    assert won?(board, player_one_mark())
+  end
+
+  test "get all indices" do
+    assert get_all_indices(@empty_board) == [
+             [0, 1, 2],
+             [3, 4, 5],
+             [6, 7, 8],
+             [0, 3, 6],
+             [1, 4, 7],
+             [2, 5, 8]
+           ]
+  end
+
+  test "get row indices" do
+    assert row_indices(@empty_board) == [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+  end
+
+  test "get col indices" do
+    assert col_indices(@empty_board) == [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
+  end
+
+  def generate_marked_board(empty_board, positions, mark) do
+    board_indices = 0..length(empty_board)
+    marks_with_indices = Enum.zip(empty_board, board_indices)
+    Enum.map(marks_with_indices, fn mark_with_index -> 
+      if Enum.member?(positions, elem(mark_with_index, 1)) do
+        mark
+      else
+        :empty_mark
+      end
+    end)
+  end
+
+  test "test function - generate marked board" do
+    assert generate_marked_board(@empty_board, [0, 1, 2], player_one_mark()) == 
+      @empty_board |> List.replace_at(0, player_one_mark())
+       |> List.replace_at(1, player_one_mark())
+       |> List.replace_at(2, player_one_mark())
   end
 end

fd8e6fbfdbc40f9a8eb9dfbd77b58ec144f311c5
the-everlearner <devlin.glasman@gmail.com>
2019-01-10 16:56:21 +0000

    add score board fun

--- lib/comp_player.ex
+++ lib/comp_player.ex
@@ -1,8 +1,28 @@
 defmodule CompPlayer do
-  import Board, only: [place_mark: 3]
+  import Board, only: [place_mark: 3, won?: 2]
+  import Marks
 
   def make_comp_move(board, player_mark) do
-    move = Enum.random(0..length(board) - 1)
+    move = get_best_tile(board, player_mark)
     place_mark(board, move, player_mark)
+  end
+
+  def get_best_tile(board, player_mark) do
+  end
+
+  def score_board(board, scorer, opponent) do
+    cond do
+      won?(board, scorer) -> 10
+      won?(board, opponent) -> 10
+      true -> 0
+    end
+  end
+
+  def get_opponent(mark) do
+    if mark == player_one_mark() do
+      player_two_mark()
+    else
+      player_one_mark()
+    end
   end
 end
--- test/comp_player_test.exs
+++ test/comp_player_test.exs
@@ -6,7 +6,24 @@ defmodule CompPlayerTest do
 
 @empty_board List.duplicate(empty_mark(), 9)
 
-  test "returns a tile number" do
-    board = generate_marked_board(@empty_board, [0], player_one_mark())
+  test "retrieve opposite player mark" do
+    assert get_opponent(player_one_mark()) == player_two_mark()
+    assert get_opponent(player_two_mark()) == player_one_mark()
+  end
+
+  test "scores board positively for win" do
+    p1_won_board = generate_marked_board(@empty_board, [0, 1, 2], player_one_mark())
+    
+    assert score_board(p1_won_board, player_one_mark(), player_two_mark()) == 10
+  end
+
+  test "scores board negatively for loss" do
+    p1_won_board = generate_marked_board(@empty_board, [0, 1, 2], player_two_mark())
+    
+    assert score_board(p1_won_board, player_two_mark(), player_one_mark()) == 10
+  end
+
+  test "scores board 0 if neither win nor loss" do
+    assert score_board(@empty_board, player_two_mark(), player_one_mark()) == 0
   end
 end

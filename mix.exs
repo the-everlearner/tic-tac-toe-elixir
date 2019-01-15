a519c7efdeec36cd81b21278e6d753f528334963
the-everlearner <devlin.glasman@gmail.com>
2019-01-15 10:22:28 +0000

    Remove unnecessary files

--- mix.exs
+++ mix.exs
@@ -16,19 +16,15 @@ defmodule TicTacToe.MixProject do
     [main_module: Main, embed_elixir: true]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:mix_test_watch, "~> 0.8", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
